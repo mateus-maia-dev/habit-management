9c3f2e93530cb278d96c95b1e8e4849bf6a8173e
mateus maia <eng.fonsecamateus@gmail.com>
2021-03-10 14:25:52 -0300

    trying to create header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { Routes } from "./routes";
+import { Header } from "./components/Header";
+import { TemporaryDrawer } from "./components/Header";
+import GlobalStyles from "./styles/global";
 
 function App() {
   return (
     <div className="App">
+      {/* <Header /> */}
+      <TemporaryDrawer />
       <Routes />
+      <GlobalStyles />
     </div>
   );
 }
