f112eccfc1fdf766659165befffd5dd3d87bf68c
mateus maia <eng.fonsecamateus@gmail.com>
2021-03-16 18:48:52 -0300

    resolvendo conflitos

--- src/components/OneGroup/index.js
+++ src/components/OneGroup/index.js
@@ -29,20 +29,11 @@ const data = {
 const OneGroup = () => {
   /*
   const [group, setGroup] = useState({});
-<<<<<<< HEAD
-
-  const { groupIDReducer } = useSelector(state => state);
-
-  const getOneGroup = () => {
-    api
-      .get(`/groups/${groupIDReducer}/`, {
-=======
   const groupID = useSelector((state) => state.groupIDReducer);
 
   const getOneGroup = () => {
     api
       .get(`/groups/3/`, {
->>>>>>> d8898d1acf463c31f8250bd5c2cc306d706b6953
         headers: {
           Authorization: `Bearer ${token}`,
         },
@@ -53,37 +44,6 @@ const OneGroup = () => {
 
   useEffect(() => {
     getOneGroup();
-<<<<<<< HEAD
-  }, [groupIDReducer]);
-
-  return (
-    <PageWrapper>
-      <h1>{group.name}</h1>
-      <span>
-        <CardContainer>
-          <CardHeader>
-            <h6>{group.user ? group.users.length : "0"} inscritos </h6>
-            <p>{group.description}</p>
-          </CardHeader>
-
-          <h2>metas</h2>
-
-          {group.goals && group.goals.map((goal, index) => (
-            <CardList key={index}>
-              <p>{goal.title} </p>
-              <span>
-                <p>Dificuldade:</p>
-                <p>{goal.difficulty} </p>
-              </span>
-              <span>
-                <p>Progresso: </p>
-                <p>{goal.how_much_achieved}</p>
-              </span>
-            </CardList>
-          ))}
-        </CardContainer>
-
-=======
   }, []);
 
   console.log(group);
@@ -94,7 +54,6 @@ const OneGroup = () => {
         <h1>gruop.name</h1>
       </Title>
       <DisplayF>
->>>>>>> d8898d1acf463c31f8250bd5c2cc306d706b6953
         <Graph>
           <Doughnut data={data} />
           <br></br>
