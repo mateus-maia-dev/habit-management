053d809dfba2deaff4a7b8f06860b57fa773c39f
Marcelo Semeghini <marcelosemeghini@gmail.com>
2021-03-18 18:12:06 -0300

    group subscription

--- src/components/Groups/GroupCard.jsx
+++ src/components/Groups/GroupCard.jsx
@@ -1,11 +1,19 @@
+import { useEffect } from "react";
+import { useState } from "react";
 import api from "../../services/api";
 import { CardContainer, ContentCard } from "./CardStyle";
 // import { useDispatch } from "react-redux";
 // import { getOneGroupThunk } from "../../store/modules/getOneGroup/thunk";
 // import { useHistory } from "react-router-dom";
 
-const GroupCard = ({ item, handleId }) => {
+const GroupCard = ({ item, handleId, setMyGroup, myGroup }) => {
   const token = localStorage.getItem("token");
+  const [ subscribed, setSubscribed ] = useState(false)
+  
+  useEffect(() => {
+   
+    Number(myGroup.id) === Number(item.id) ? setSubscribed(true) : setSubscribed(false)
+  }, [myGroup])
 
   const handleClick = () => {
     api.post(
@@ -15,6 +23,7 @@ const GroupCard = ({ item, handleId }) => {
         headers: { Authorization: `Bearer ${token}` },
       }
     );
+    setMyGroup(item)
   };
 
   return (
@@ -24,7 +33,11 @@ const GroupCard = ({ item, handleId }) => {
         <h3>Categoria: {item.category}</h3>
         <h3>{item.description}</h3>
       </ContentCard>
+      {subscribed ?
+      <button disabled={true} style={{backgroundColor:"white", color:"#2d2d2d"}}>Inscrito</button>
+      :
       <button onClick={() => handleClick()}>Se inscrever</button>
+      }
       <button onClick={() => handleId(item.id)}>Mais informacoes</button>
     </CardContainer>
   );
