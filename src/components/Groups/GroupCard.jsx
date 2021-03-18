ee7c971eae15b72b27ee4250e9d0872775890289
mateus maia <eng.fonsecamateus@gmail.com>
2021-03-18 19:23:26 -0300

    responsividade da mensagem de saudacao

--- src/components/Groups/GroupCard.jsx
+++ src/components/Groups/GroupCard.jsx
@@ -1,5 +1,8 @@
 import api from "../../services/api";
 import { CardContainer, ContentCard } from "./CardStyle";
+import Alert from "@material-ui/lab/Alert";
+import { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
 // import { useDispatch } from "react-redux";
 // import { getOneGroupThunk } from "../../store/modules/getOneGroup/thunk";
 // import { useHistory } from "react-router-dom";
@@ -8,31 +11,35 @@ const GroupCard = ({ item, handleId }) => {
   const token = localStorage.getItem("token");
 
   const handleClick = () => {
-    api.post(
-      `/groups/${item.id}/subscribe/`,
-      { key: true },
-      {
-        headers: { Authorization: `Bearer ${token}` },
-      }
-    );
+    api
+      .post(
+        `/groups/${item.id}/subscribe/`,
+        { key: true },
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      )
+      .catch((e) => console.log(e));
   };
 
   return (
-    <CardContainer>
-      <h2>{item.title}</h2>
-      <ContentCard>
-        <h2>{item.name}</h2>
-        <div>
-          <p>Categoria:</p>
-          <p>{item.category}</p>
-        </div>
-        <div>
-          <p>{item.description}</p>
-        </div>
-      </ContentCard>
-      <button onClick={() => handleClick()}>Se inscrever</button>
-      <button onClick={() => handleId(item.id)}>Mais informacoes</button>
-    </CardContainer>
+    <>
+      <CardContainer>
+        <h2>{item.title}</h2>
+        <ContentCard>
+          <h2>{item.name}</h2>
+          <div>
+            <p>Categoria:</p>
+            <p>{item.category}</p>
+          </div>
+          <div>
+            <p>{item.description}</p>
+          </div>
+        </ContentCard>
+        <button onClick={() => handleClick()}>Se inscrever</button>
+        <button onClick={() => handleId(item.id)}>Mais informacoes</button>
+      </CardContainer>
+    </>
   );
 };
 
--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -2,43 +2,36 @@ import { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
 import api from "../../services/api";
-import { userGroupRequestThunk } from "../../store/modules/groupReduce/thunk";
-
 import {
   Container,
   HeaderLine,
   CardContainer,
   ContentCard,
   ImgDashboard,
+  Greetings,
 } from "./style";
-
 import { useDispatch } from "react-redux";
 import { requestHabitThunk } from "../../store/modules/habitReduce/thunk";
-
 import HabitsList from "../../components/PersonalHabits/HabitsList";
 import CreateHabit from "../../components/CreateHabit/index";
 import jwt_decoded from "jwt-decode";
-
-import OneGroup from "../../components/OneGroup";
+//import GroupList from "../../components/Groups/GroupList";
+import GroupList from "../../components/Groups/GroupList";
+import Button from "../../components/Buttons/index";
 
 const Dashboard = () => {
   const dispatch = useDispatch();
-
   const [userData, setUserData] = useState([]);
-  // const [groups, setGroups] = useState([]);
-  const [myGroup, setMyGroup] = useState([]);
-  console.log(userData);
-  // console.log(myGroup);
-
+  const [groups, setGroups] = useState([]);
+  console.log(groups);
   const token = useSelector((state) => state.signInReducer);
   const decoded = jwt_decoded(token.token);
-
-  // const group = useSelector((state) => state.groupIDReducer);
-
-  // console.log(group);
-
+  // console.log(userData);
+  const getGroups = () => {
+    api.get("/groups/").then((response) => setGroups(response.data.results));
+  };
+  const [myGroup, setMyGroup] = useState("");
   const history = useHistory();
-
   const handleUserData = () => {
     api
       .get(`/users/${decoded.user_id}/`, {
@@ -47,48 +40,33 @@ const Dashboard = () => {
         },
       })
       .then((response) => setUserData(response.data));
-
-    // dispatch(userGroupRequestThunk(userData))
   };
-
+  console.log("myGroup", myGroup);
   const userPersonalHabits = useSelector(
     (state) => state.changeHabitReduce.userData
   );
-
   const changeReduce = useSelector((state) => state.changeHabitReduce.change);
 
-  async function getUserGroup() {
-    await api
-      .get(`/groups/${userData.group}/`, {
-        headers: {
-          Authorization: `Bearer ${token.token}`,
-        },
-      })
-      .then((response) => setMyGroup(response.data));
-  }
-
   useEffect(() => {
-    console.log("entrou1");
     dispatch(requestHabitThunk());
     handleUserData();
-    console.log(userData.group);
+    getGroups();
     // eslint-disable-next-line
   }, []);
 
   useEffect(() => {
-    console.log("entrou2");
-
     dispatch(requestHabitThunk());
   }, [changeReduce]);
 
   useEffect(() => {
-    console.log("entrou3");
-    if (!!userData.group) {
-      getUserGroup();
-    }
-
-    // dispatch(userGroupRequestThunk(userData));
-
+    userData.group &&
+      api
+        .get(`/groups/${userData.group}/`, {
+          headers: {
+            Authorization: `Bearer ${token.token}`,
+          },
+        })
+        .then((response) => setMyGroup(response.data));
     // eslint-disable-next-line
   }, [userData]);
 
@@ -102,21 +80,21 @@ const Dashboard = () => {
     // </Container>
     <ImgDashboard>
       <Container>
+        <Greetings>
+          Bem-vindo &ensp;<span>{userData.username}</span>{" "}
+        </Greetings>
         <HeaderLine>
           <h1>MEUS HÁBITOS</h1>
           <CreateHabit />
         </HeaderLine>
         <HabitsList items={userPersonalHabits} />
-
         <HeaderLine>
           <h1>MEUS GRUPOS</h1>
-
           <button onClick={() => history.push("/groups")}>
             Pesquisar grupos
           </button>
         </HeaderLine>
-
-        {/* {userData.group && (
+        {userData.group && (
           <CardContainer>
             <ContentCard>
               <h2>{myGroup.name}</h2>
@@ -125,17 +103,11 @@ const Dashboard = () => {
               <p>{myGroup.goals && myGroup.goals.length} metas</p>
             </ContentCard>
           </CardContainer>
-        ) : (
-          <div>Nao Tem Grupo</div>
         )}
-        {!userData.group && <div>Nao Tem Grupo</div>} */}
-
+        {!userData.group && <div>Nao Tem Grupo</div>}
         {/* <GroupList /> */}
-        {/* <CreateHabit /> */}
-        {/* <OneGroup /> */}
       </Container>
     </ImgDashboard>
   );
 };
-
 export default Dashboard;
