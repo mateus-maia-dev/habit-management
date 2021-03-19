d8eb77c0610a1e30ab47e8613b7395201bab48cd
Joao Manoel Ribas <joaoribas35@hotmail.com>
2021-03-18 23:54:06 -0300

    agora vai

--- src/components/PersonalHabits/CardStyle.js
+++ src/components/PersonalHabits/CardStyle.js
@@ -21,24 +21,23 @@ export const CardContainer = styled.div`
   }
 
   button {
-    width: 70%;
-
+    width: 150px;
+    border: none;
+    margin: 5px 0;
     display: flex;
     flex-direction: row;
     justify-content: center;
     align-items: center;
+    margin-top: 10px;
+    padding: 5px 10px;
+    border: none;
+    background-color: #bebebe;
 
-    background: #f1f1f1; //#3a3a3a;
-    cursor: pointer;
-    text-align: center;
     border-radius: 15px;
-    border: 0px;
-    color: #3a3a3a;
-    //height: 30px;
-    padding: 0 30px;
-    box-shadow: 0 3px 5px 2px rgba(0, 0, 0, 0.3);
-    font-size: 1rem;
-    margin: 6% 0% 2% 0;
+
+    &:hover {
+      background-color: #bc494c;
+    }
   }
 `;
 
@@ -73,54 +72,23 @@ export const ContentButtons = styled.div`
   padding: 5px 0;
 `;
 
-// export const ContentCard = styled.div`
-//   width: 90%;
-//   margin: 0 auto;
-
-//   display: flex;
-//   flex-direction: column;
-//   justify-content: center;
-//   align-items: center;
-
-//   h2,
-//   h3,
-//   p {
-//     padding: 15px;
-//   }
-
-//   h2 {
-//     color: #bc494b;
-//   }
-// `;
-
-// export const CardContainer = styled.div`
-//   width: 300px;
-//   height: auto;
-//   margin: 3% 3%;
-//   border-radius: 30px;
-//   background: #2d2d2d;
-//   color: #fff;
-//   box-shadow: 5px 5px 15px 5px #000000;
-
-//   text-align: center;
-
-//   button {
-//     background: #3a3a3a;
-//     cursor: pointer;
-//     text-align: center;
-//     border-radius: 15px;
-//     border: 0px;
-//     color: #fff;
-//     height: 30px;
-//     padding: 0 30px;
-//     box-shadow: 0 3px 5px 2px rgba(0, 0, 0, 0.3);
-//     font-size: 1rem;
-//     margin-top: 10px;
-//     margin-bottom: 20px;
-//   }
-// `;
-
 export const ButtonsContainer = styled.div`
   display: flex;
   flex-direction: column;
+  button {
+    width: 150px;
+    border: none;
+    margin: 5px 0;
+
+    margin-top: 10px;
+    padding: 5px 10px;
+    border: none;
+    background-color: #bebebe;
+
+    border-radius: 15px;
+
+    &:hover {
+      background-color: #bc494c;
+    }
+  }
 `;
